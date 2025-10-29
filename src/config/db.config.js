96bf51931a5233b506020738b51e65db31a49ad1
moy <martinezmoy919@gmail.com>
2025-10-29 15:30:07 -0600

    modificacion 16

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -14,7 +14,8 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
   enableKeepAlive: true,
-  keepAliveInitialDelay: 0
+  keepAliveInitialDelay: 0,
+  ssl: false
 });
 
 // Exportar el pool para usar en los modelos
