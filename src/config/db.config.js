7377b159eecd02c096fd9bf59b34a9ddcba1faa1
moy <martinezmoy919@gmail.com>
2025-10-29 10:48:07 -0600

    modificacion 3

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -5,11 +5,11 @@ dotenv.config();
 
 // Configuración del pool de conexiones MySQL
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'tu_base_de_datos',
-  port: process.env.DB_PORT || 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
